f79989eea262a2893b84c53787d278eeba1fc958
Ju <justine.trupheme@epitech.eu>
2022-04-29 10:32:08 +0200

    oui

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,8 @@ import Klusters
 import System.Exit
 import Data.Maybe
 import System.Random
+import System.IO
+import Control.Exception
 
 main :: IO ()
 main = do
@@ -16,5 +18,6 @@ main = do
     else return ()
     let vArgs = fromJust args
     g <- newStdGen
+    contents <- catch (readFile(getF vArgs)) (\e -> putStrLn "File not found" >> exitWith (ExitFailure 84))
     let klusters = createNKlusters (getN vArgs) $ genNRandomColorsF (getN vArgs) 0 g (defColor)
     mapM_ (\k -> putStr (show k)) klusters
