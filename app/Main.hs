7a7fe9472d62afd2503847e716be950df076b611
maxnce <maxence.pellouin@epitech.eu>
2022-05-02 10:30:25 +0200

    [Add] - File parsing implemented

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Lib
 import System.Environment
 import Args
+import File
 import Klusters
 import System.Exit
 import Data.Maybe
@@ -18,6 +19,8 @@ main = do
     else return ()
     let vArgs = fromJust args
     g <- newStdGen
-    contents <- catch (readFile(getF vArgs)) (\e -> putStrLn "File not found" >> exitWith (ExitFailure 84))
+    contents <- catch (readFile (getF vArgs)) catchFileError
+    let pixels = parsePixels contents
+    print pixels
     let klusters = createNKlusters (getN vArgs) $ genNRandomColorsF (getN vArgs) 0 g (defColor)
     mapM_ (\k -> putStr (show k)) klusters
---
+++ src/File.hs
@@ -0,0 +1,25 @@
+module File
+    ( 
+        catchFileError,
+        nextChar,
+        beforeChar,
+        contentsToLines
+    ) where
+
+import Control.Exception
+import System.Exit
+import Data.List
+
+catchFileError :: SomeException -> IO String
+catchFileError e = putStrLn "Error reading file" >> exitWith (ExitFailure 84)
+
+contentsToLines :: String -> [String]
+contentsToLines = lines
+
+nextChar :: Char -> String -> String
+nextChar c [] = []
+nextChar c (x:xs) = if c == x then (x:xs) else nextChar c xs
+
+beforeChar :: Char -> String -> String
+beforeChar _ [] = []
+beforeChar c (x:xs) = if c == x then [c] else x : beforeChar c xs
